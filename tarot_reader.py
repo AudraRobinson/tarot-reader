6e890fa2354d13b9e0af4ab185526e8dc011fb8d
AudraRobinson <audrageorge1999@gmail.com>
2023-02-01 20:13:42 -0600

    Add files via upload

---
+++ tarot_reader.py
@@ -0,0 +1,11 @@
+"""
+   Tarot Card Reading:
+   1. Ask a question.
+   2. Shuffle deck.
+   3. Draw 3 cards.
+   4. Interpret in a 3 card spread.
+"""
+import random as rand
+
+class Card:
+   pass
